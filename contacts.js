adc891dfc2fe270f8891efa58d138c86047b7b70
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-12 20:22:50 +0100

    contacts js functions add

--- contacts.js
+++ contacts.js
@@ -1,2 +1,72 @@
 const fs = require("fs");
 const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const nanoid = require("nanoid");
+
+function listContacts() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(contactsPath, "utf-8", (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      try {
+        const contacts = JSON.parse(data);
+        resolve(contacts);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
+
+function getContactById(contactId) {
+  return listContacts()
+    .then((contacts) => contacts.find((contact) => contact.id === contactId))
+    .catch((error) => console.error(error));
+}
+
+function removeContact(contactId) {
+  return listContacts()
+    .then((contacts) => {
+      const updatedContacts = contacts.filter(
+        (contact) => contact.id !== contactId
+      );
+      return new Promise((resolve, reject) => {
+        fs.writeFile(
+          contactsPath,
+          JSON.stringify(updatedContacts, null, 2),
+          (err) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(updatedContacts);
+          }
+        );
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+function addContact(name, email, phone) {
+  return listContacts()
+    .then((contacts) => {
+      const newContact = { id: nanoid(), name, email, phone };
+      contacts.push(newContact);
+      return new Promise((resolve, reject) => {
+        fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(newContact);
+        });
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Command } = require("commander");
 const program = new Command();
+const contacts = require("./db/contacts.json");
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,3 +12,28 @@ program
 program.parse(process.argv);
 
 const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
