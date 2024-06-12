477200d8e43cc9be2479d77ee7526c45f1a46415
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-12 20:52:25 +0100

    contacts js add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Command } = require("commander");
-const program = new Command();
+require("color");
 const contacts = require("./db/contacts.json");
+const program = new Command();
 
 program
   .option("-a, --action <type>", "choose action")
@@ -13,22 +14,46 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-function invokeAction({ action, id, name, email, phone }) {
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      try {
+        const allContacts = await contacts.listContacts();
+        console.table(allContacts.green);
+      } catch (error) {
+        console.error("Error listing contacts: ", error);
+      }
       break;
 
     case "get":
-      // ... id
+      try {
+        const contact = await contacts.getContactById(id);
+        if (contact) {
+          console.log(contact.blue);
+        } else {
+          console.log(`Contact with id ${id} not found`);
+        }
+      } catch (error) {
+        console.error("Error getting contact by id: ", error);
+      }
       break;
 
     case "add":
-      // ... name email phone
+      try {
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact.yellow);
+      } catch (error) {
+        console.error("Error adding contact: ", error);
+      }
       break;
 
     case "remove":
-      // ... id
+      try {
+        const updatedContacts = await contacts.removeContact(id);
+        console.log(updatedContacts.orange);
+      } catch (error) {
+        console.error("Error removing contact: ", error);
+      }
       break;
 
     default:
