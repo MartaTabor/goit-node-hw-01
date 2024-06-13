9805152f38095930c7b8eacc74ac3f6c232fde85
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-13 19:07:03 +0100

    id generator update

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
@@ -39,7 +40,7 @@ async function removeContact(contactId) {
 async function addContact(name, email, phone) {
   try {
     const contacts = await listContacts();
-    const newContact = { id: Date.now().toString(), name, email, phone };
+    const newContact = { id: uuidv4(), name, email, phone };
     contacts.push(newContact);
     fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return newContact;
